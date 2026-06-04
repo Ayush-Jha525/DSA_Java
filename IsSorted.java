a4e4c95dc03f07e67e5226c1d408f22c92cd2096
Ayush-Jha525 <ayush820j@gmail.com>
2026-06-04 08:12:41 +0530

    Array is sorted or not

---
+++ IsSorted.java
@@ -0,0 +1,25 @@
+//Question - Array is Sorted or not
+//Time Complexity - O(n)
+//Space Complexity - O(1)
+
+public class IsSorted {
+    public static boolean isSorted(int[] arr) {
+        if (arr.length <= 1) {
+            return true;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = { 5, 2, 8, 1, 9, 12, 7 };
+        int[] arr2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        System.out.println(isSorted(arr1));
+        System.out.println(isSorted(arr2));
+    }
+}
